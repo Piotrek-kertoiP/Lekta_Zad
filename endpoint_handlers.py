4aa6cea46d233a214d7cd5bc39ed6d0ee84525f3
piotrek <piotrek.szczerbinski@gmail.com>
2020-06-16 01:33:19 +0200

    Added a function to remove redundant parentheses (1) -> 1

--- endpoint_handlers.py
+++ endpoint_handlers.py
@@ -1,5 +1,6 @@
 from custom_exceptions import ParenthesesError, UnallowedCharacterError, MissingOperationArgumentError, InvalidExpressionError
 
+verbose = True
 '''These are functions that may be commonly used'''
 def is_operator(char):
     if char == "+" or char == "-" or char == '/' or char == "*":
@@ -23,7 +24,9 @@ class RequestValidator:
         self.expr = request.json["expression"]
 
     def omit_whitespaces(self):
+        if verbose: print("omit_whitespaces, start:\t\t\t " + self.expr)
         self.expr = self.expr.replace(" ", "")
+        if verbose: print("omit_whitespaces, end:\t\t\t\t " + self.expr)
 
     def check_parentheses(self):
         counter = 0
@@ -46,13 +49,14 @@ class RequestValidator:
 
     # this function changes unary minus like (-1) to (0-1); we'll check it later if it's proper unary minus (not (-*))
     def change_unary_minus(self):
+        if verbose: print("change_unary_minus, start:\t\t\t " + self.expr)
         for i in range(0, len(self.expr) - 1):
             if self.expr[i] == "(" and self.expr[i+1] == "-":
                 self.expr = self.expr[:i+1] + "0" + self.expr[i+1:]
+        if verbose: print("change_unary_minus, end:\t\t\t " + self.expr)
 
     # this function checks if every operator is surrounded with 2 digits after getting rid of unary minuses
     def check_operator_neighbours(self):
-        print(self.expr)
         # check first and last character to avoid indexing out of bounds
         expr_len = len(self.expr)
         first_char = self.expr[0]
@@ -66,6 +70,8 @@ class RequestValidator:
         return True
 
     def add_missing_parentheses(self):
+        if verbose: print("add_missing_parentheses, start:\t\t " + self.expr)
+
         mul_div_index = 0
         while mul_div_index < len(self.expr):
             if self.expr[mul_div_index] == "*" or self.expr[mul_div_index] == "/":
@@ -74,14 +80,14 @@ class RequestValidator:
                     left_index = mul_div_index - 1
                     while left_index > 0 and is_digit(self.expr[left_index]):
                         left_index += -1
-                    self.expr = self.expr[0:left_index + 1] + "(" + self.expr[left_index + 1:mul_div_index] + ")" + self.expr[mul_div_index:]
+                    self.expr = self.expr[:left_index + 1] + "(" + self.expr[left_index + 1:mul_div_index] + ")" + self.expr[mul_div_index:]
                     mul_div_index += 2
                 # wrap right argument
                 if mul_div_index + 1 <= len(self.expr) and not self.expr[mul_div_index + 1] == "(":
                     right_index = mul_div_index + 1
                     while right_index < len(self.expr) and is_digit(self.expr[right_index]):
                         right_index += 1
-                    self.expr = self.expr[0:mul_div_index + 1] + "(" + self.expr[mul_div_index + 1:right_index] + ")" + self.expr[right_index:]
+                    self.expr = self.expr[:mul_div_index + 1] + "(" + self.expr[mul_div_index + 1:right_index] + ")" + self.expr[right_index:]
                     mul_div_index += 2
             mul_div_index += 1
 
@@ -105,7 +111,7 @@ class RequestValidator:
                     elif self.expr[right_index] == "(":
                         right_arg_parenth_cntr += 1
                     right_index += 1
-                self.expr = self.expr[0:left_index+1] + "(" + self.expr[left_index+1:right_index] + ")" + self.expr[right_index:]
+                self.expr = self.expr[:left_index+1] + "(" + self.expr[left_index+1:right_index] + ")" + self.expr[right_index:]
                 mul_div_index += 1
             mul_div_index += 1
 
@@ -117,14 +123,14 @@ class RequestValidator:
                     left_index = add_substr_index - 1
                     while left_index > 0 and is_digit(self.expr[left_index]):
                         left_index += -1
-                    self.expr = self.expr[0:left_index + 1] + "(" + self.expr[left_index + 1:add_substr_index] + ")" + self.expr[add_substr_index:]
+                    self.expr = self.expr[:left_index + 1] + "(" + self.expr[left_index + 1:add_substr_index] + ")" + self.expr[add_substr_index:]
                     add_substr_index += 2
                 # wrap right argument
                 if add_substr_index + 1 <= len(self.expr) and not self.expr[add_substr_index + 1] == "(":
                     right_index = add_substr_index + 1
                     while right_index < len(self.expr) and is_digit(self.expr[right_index]):
                         right_index += 1
-                    self.expr = self.expr[0:add_substr_index + 1] + "(" + self.expr[add_substr_index + 1:right_index] + ")" + self.expr[right_index:]
+                    self.expr = self.expr[:add_substr_index + 1] + "(" + self.expr[add_substr_index + 1:right_index] + ")" + self.expr[right_index:]
                     add_substr_index += 2
             add_substr_index += 1
 
@@ -148,13 +154,25 @@ class RequestValidator:
                     elif self.expr[right_index] == "(":
                         right_arg_parenth_cntr += 1
                     right_index += 1
-                self.expr = self.expr[0:left_index + 1] + "(" + self.expr[left_index + 1:right_index] + ")" + self.expr[right_index:]
+                self.expr = self.expr[:left_index + 1] + "(" + self.expr[left_index + 1:right_index] + ")" + self.expr[right_index:]
                 add_substr_index += 1
             add_substr_index += 1
-
+        if verbose: print("add_missing_parentheses, end:\t\t " + self.expr)
 
     def remove_redundant_parentheses(self):
-        pass
+        if verbose: print("remove_redundant_parentheses, start: " + self.expr)
+        index = 1
+        while index < len(self.expr) - 1:
+            if is_digit(self.expr[index]) and self.expr[index - 1] == "(":
+                left_parenth_index = index - 1
+                while not self.expr[index] == ")":
+                    index += 1
+                right_parenth_index = index
+                if is_number(self.expr[left_parenth_index + 1:right_parenth_index - 1]):
+                    self.expr = self.expr[:left_parenth_index] + self.expr[left_parenth_index + 1:right_parenth_index] + self.expr[right_parenth_index + 1:]
+                    index += -1
+            index += 1
+        if verbose: print("remove_redundant_parentheses, end:\t " + self.expr)
 
     def validate_request(self):
         self.omit_whitespaces()
@@ -169,8 +187,6 @@ class RequestValidator:
         self.remove_redundant_parentheses()
         print("validate_request: " + self.expr)
         return self.expr
-
-
 
 class ExpressionEvaluator:
     def __init__(self, expr):
